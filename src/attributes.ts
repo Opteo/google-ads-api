d6181d40f4c785f5155819f94a11f2fbbe598197
Mateusz <mateuszjanusz95@gmail.com>
2018-09-18 16:55:56 +0100

    ad_group, ad_group_criterion, campaign

--- src/attributes.ts
+++ src/attributes.ts
@@ -1,5 +1,4 @@
 
-
 const text_ad = {
     headline: '',
     description1: '',
@@ -53,71 +52,108 @@ const ad =  {
     shopping_smart_ad: ''
 }
 
+
+const quality_info = {
+    quality_score: '',
+    creative_quality_score: '',
+    post_click_quality_score: '',
+    search_predicted_ctr: '',
+}
+const position_estimates = {
+    first_page_cpc_micros: '',
+    first_position_cpc_micros: '',
+    top_of_page_cpc_micros: '',
+}
+
+
+const network_settings = {
+    target_google_search: '',
+    target_search_network: '',
+    target_content_network: '',
+    target_partner_search_network: '',
+}
+const hotel_setting = {
+    hotel_center_id: ''
+}
+const dynamic_search_ads_setting = {
+    domain_name: '',
+    language_code: '',
+    use_supplied_urls_only: '',
+    feed_ids: ''
+}
+const shopping_setting = {
+    merchant_id: '',
+    sales_country: '',
+    campaign_priority: '',
+    enable_local: ''
+}
+
+
 const entities : { [key: string]: object } = {
     ad_group_ad: {
         status: '',
         ad_group: '',
         ad
     },
-    // 'ad_group': [
-    //     'id',
-    //     'name',
-    //     'status',
-    //     'type',
-    //     'tracking_url_template',
-    //     'url_custom_parameters',
-    //     'campaign',
-    //     'cpc_bid_micros',
-    //     'cpm_bid_micros',
-    //     'cpa_bid_micros',
-    //     'cpv_bid_micros',
-    //     'target_roas_override',
-    //     'percent_cpc_bid_micros',
-    // ],
-    // 'ad_group_criterion': [
-    //     'criterion_id',
-    //     'status',
-    //     // 'quality_info',
-    //     'ad_group',
-    //     'type',
-    //     'negative',
-    //     'cpc_bid_micros',
-    //     'cpm_bid_micros',
-    //     'cpv_bid_micros',
-    //     'percent_cpc_bid_micros',
-    //     'effective_cpc_bid_micros',
-    //     'effective_cpm_bid_micros',
-    //     'effective_cpv_bid_micros',
-    //     'effective_percent_cpc_bid_micros',
-    //     'effective_cpc_bid_source',
-    //     'effective_cpm_bid_source',
-    //     'effective_cpv_bid_source',
-    //     'effective_percent_cpc_bid_source',
-    //     // 'position_estimates',
-    //     'final_urls',
-    //     'tracking_url_template',
-    //     'url_custom_parameters',
-    // ],
-    // campaign: [
-    //     'id',
-    //     'name',
-    //     'status',
-    //     'serving_status',
-    //     'ad_serving_optimization_status',
-    //     'advertising_channel_type',
-    //     'advertising_channel_sub_type',
-    //     'tracking_url_template',
-    //     'url_custom_parameters',
-    //     // 'network_settings',
-    //     // 'hotel_setting',
-    //     // 'dynamic_search_ads_setting',
-    //     // 'shopping_setting',
-    //     'campaign_budget',
-    //     'bidding_strategy_type',
-    //     'start_date',
-    //     'campaign_group',
-    //     'end_date',
-    // ],
+    ad_group: {
+        id: '',
+        name: '',
+        status: '',
+        type: '',
+        tracking_url_template: '',
+        url_custom_parameters: '',
+        campaign: '',
+        cpc_bid_micros: '',
+        cpm_bid_micros: '',
+        cpa_bid_micros: '',
+        cpv_bid_micros: '',
+        target_roas_override: '',
+        percent_cpc_bid_micros: '',
+    },
+    ad_group_criterion: {
+        criterion_id: '',
+        status: '',
+        quality_info,
+        ad_group: '',
+        type: '',
+        negative: '',
+        cpc_bid_micros: '',
+        cpm_bid_micros: '',
+        cpv_bid_micros: '',
+        percent_cpc_bid_micros: '',
+        effective_cpc_bid_micros: '',
+        effective_cpm_bid_micros: '',
+        effective_cpv_bid_micros: '',
+        effective_percent_cpc_bid_micros: '',
+        effective_cpc_bid_source: '',
+        effective_cpm_bid_source: '',
+        effective_cpv_bid_source: '',
+        effective_percent_cpc_bid_source: '',
+        position_estimates,
+        final_urls: '',
+        tracking_url_template: '',
+        url_custom_parameters: '',
+    },
+    campaign: {
+        id: '',
+        name: '',
+        status: '',
+        serving_status: '',
+        ad_serving_optimization_status: '',
+        advertising_channel_type: '',
+        advertising_channel_sub_type: '',
+        tracking_url_template: '',
+        url_custom_parameters: '',
+        network_settings,
+        hotel_setting,
+        dynamic_search_ads_setting,
+        shopping_setting,
+        campaign_budget: '',
+        bidding_strategy_type: '',
+        start_date: '',
+        campaign_group: '',
+        end_date: '',
+    },
     // 'campaign_budget': [
     //     'id',
     //     'name',
