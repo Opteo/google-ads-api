c549fbd5cf9ba9d84c5c40b0ccf364434bd0acdd
jafaircl <jafaircl@me.com>
2019-02-05 15:48:23 -0500

    update NewCampaign types

--- src/types/Campaign.ts
+++ src/types/Campaign.ts
@@ -90,6 +90,35 @@ declare namespace Campaign {
         SHOPPING = 'SHOPPING',
         UNKNOWN = 'UNKNOWN',
         UNSPECIFIED = 'UNSPECIFIED',
+        VIDEO = 'VIDEO',
+    }
+    
+    /**
+     * Enum for AdvertisingChannelSubType
+     * @readonly
+     * @enum {string}
+     */
+    enum AdvertisingChannelSubType {
+        DISPLAY_EXPRESS = 'DISPLAY_EXPRESS',
+        DISPLAY_GMAIL_AD = 'DISPLAY_GMAIL_AD',
+        DISPLAY_MOBILE_APP = 'DISPLAY_MOBILE_APP',
+        DISPLAY_SMART_CAMPAIGN = 'DISPLAY_SMART_CAMPAIGN',
+        SEARCH_EXPRESS = 'SEARCH_EXPRESS',
+        SEARCH_MOBILE_APP = 'SEARCH_MOBILE_APP',
+        SHOPPING_SMART_ADS = 'SHOPPING_SMART_ADS',
+        UNKNOWN = 'UNKNOWN',
+        UNSPECIFIED = 'UNSPECIFIED',
+        VIDEO_ACTION = 'VIDEO_ACTION',
+        VIDEO_OUTSTREAM = 'VIDEO_OUTSTREAM'
+    }
+    
+    /**
+     * Enum for PageOnePromotedStrategyGoal
+     * @readonly
+     * @enum {string}
+     */
+    enum PageOnePromotedStrategyGoal {
+        UNSPEC 
     }
 
     /**
@@ -110,6 +139,68 @@ declare namespace Campaign {
     export interface TargetSpend {
         cpc_bid_ceiling_micros: string | number
     }
+    
+    /**
+     * Interface for ManualCpcBiddingStrategy
+     * @interface
+     */
+    export interface ManualCpcBiddingStrategy {
+        enhanced_cpc_enabled: boolean
+    }
+    
+    /**
+     * Interface for ManualCpcBiddingStrategy
+     * @interface
+     */
+    export interface ManualCpcBiddingStrategy {
+        enhanced_cpc_enabled: boolean
+    }
+    
+    /**
+     * Interface for MaximizeConversionValueBiddingStrategy
+     * @interface
+     */
+    export interface MaximizeConversionValueBiddingStrategy {
+        target_roas: string | number
+    }
+    
+    /**
+     * Interface for PercentCpcBiddingStrategy
+     * @interface
+     */
+    export interface PercentCpcBiddingStrategy {
+        cpc_bid_ceiling_micros?: string | number
+        enhanced_cpc_enabled?: boolean
+    }
+    
+    /**
+     * Interface for TargetCpaBiddingStrategy
+     * @interface
+     */
+    export interface TargetCpaBiddingStrategy {
+        target_cpa_micros?: string | number
+        cpc_bid_ceiling_micros?: string | number
+        cpc_bid_floor_micros?: string | number
+    }
+    
+    /**
+     * Interface for TargetRoasBiddingStrategy
+     * @interface
+     */
+    export interface TargetRoasBiddingStrategy {
+        target_cpa_micros?: string | number
+        cpc_bid_ceiling_micros?: string | number
+        cpc_bid_floor_micros?: string | number
+    }
+    
+    /**
+     * Interface for TargetSpendBiddingStrategy
+     * @interface
+     */
+    export interface TargetSpendBiddingStrategy {
+        target_spend_micros?: string | number
+        cpc_bid_ceiling_micros?: string | number
+    }
 
     /**
      * Interface for NewCampaignConfig
@@ -117,8 +208,22 @@ declare namespace Campaign {
      */
     export interface NewCampaignConfig extends NewEntityConfig {
         budget_id: string
+        name: string
         advertising_channel_type: AdvertisingChannelType | keyof typeof AdvertisingChannelType
-        target_spend: TargetSpend
+        target_spend?: TargetSpend
+        status?: CampaignStatus
+        network_setting?: NetworkSettings
+        advertising_channel_sub_type?: AdvertisingChannelSubType | keyof typeof AdvertisingChannelSubType
+        manual_cpc?: ManualCpcBiddingStrategy
+        // manual_cpm?: any
+        // manual_cpv?: any
+        maximize_conversion_value?: MaximizeConversionValueBiddingStrategy
+        // maximize_conversions?: any
+        percent_cpc?: PercentCpcBiddingStrategy
+        target_cpa?: TargetCpaBiddingStrategy
+        // target_cpm?: any
+        target_roas?: TargetRoasBiddingStrategy
+        target_spend?: TargetSpendBiddingStrategy
     }
 }
 export = Campaign
