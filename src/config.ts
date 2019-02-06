2cab93e7ab4d17cfd78cc3d6e039d41475d3b255
jafaircl <jafaircl@me.com>
2019-02-06 14:37:42 -0500

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,12 @@
+const config = {
+    client_id: '',
+    client_secret: '',
+    developer_token: '',
+    cid: '',
+    manager_cid: '',
+    refresh_token: '',
+    opteo_cid: '',
+    opteo_manager_cid: '',
+    opteo_refresh_token: '',
+}
+export default config
