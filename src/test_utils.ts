8abb9dbf2ac1b59f46766708c24051e184538e26
Louis Bluhm <suspiciouspotato@gmail.com>
2019-03-18 18:16:21 +0000

    feat(test_utils): added getRandomName method

--- src/test_utils.ts
+++ src/test_utils.ts
@@ -1,6 +1,10 @@
 import GoogleAdsApi from './index'
 import config from './config'
 
+export const CID = config.cid
+export const CAMPAIGN_ID = config.testing.campaign_id
+export const BUDGET_ID = 1536143460
+
 export function newCustomer() {
     const client = new GoogleAdsApi({
         client_id: config.client_id,
@@ -9,11 +13,11 @@ export function newCustomer() {
     })
     return client.Customer({
         customer_account_id: config.cid,
-        manager_cid: config.manager_cid,
+        login_customer_id: config.login_customer_id,
         refresh_token: config.refresh_token,
     })
 }
 
-export const CID = config.cid
-export const CAMPAIGN_ID = config.testing.campaign_id
-export const BUDGET_ID = 1536143460
+export function getRandomName(entity: string) {
+    return `test-${entity}-${(Math.random() * 100000 + 1).toFixed(0)}`
+}
