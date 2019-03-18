7720f5e5d2f3e36e1bdd561f86f20c0250f05020
Louis Bluhm <suspiciouspotato@gmail.com>
2019-03-18 10:56:55 +0000

    feat(services): started work on base service

--- src/services/service.ts
+++ src/services/service.ts
@@ -3,14 +3,15 @@ import Bottleneck from 'bottleneck'
 import GrpcClient from '../grpc'
 import { formatQueryResults, buildReportQuery } from '../utils'
 import parser from '../parser'
-import { ReportConfig } from '../types/Global'
+import { ReportConfig, ServiceListOptions } from '../types/Global'
 import { SearchGrpcError } from '../Error'
+
+import * as fields from 'google-ads-node/build/lib/fields'
 
 export default class Service {
     protected cid: string
     protected client: GrpcClient
     protected service: any
-    // protected google_ads_service: any
 
     // @ts-ignore
     private throttler: Bottleneck
@@ -20,12 +21,8 @@ export default class Service {
         this.client = client
         this.throttler = throttler
 
-        // This is the specific service, e.g. "CampaignService"
+        // This is the child specific service, e.g. "CampaignService"
         this.service = client.getService(name)
-
-        // TODO: Make this is a globally available service on the GrpcClient
-        // This is "GoogleAdsService", used for search queries
-        // this.google_ads_service = client.getService('GoogleAdsService')
     }
 
     protected buildResourceName(resource: string): string {
@@ -33,19 +30,28 @@ export default class Service {
     }
 
     protected async serviceCall(call: string, request: any): Promise<any> {
-        const response = await this.service[call](request)
-        const parsed_results = this.parseServiceResults([response])
-        /* 
-            Since get returns an object, we always return the first item.
-            This should only ever be one item here, and it should exist.
-        */
-        return parsed_results[0]
+        try {
+            const response = await this.service[call](request)
+            const parsed_results = this.parseServiceResults([response])
+            /* 
+                Since get returns an object, we always return the first item.
+                This should only ever be one item here, and it should exist.
+            */
+            return parsed_results[0]
+        } catch (err) {
+            throw new SearchGrpcError(err, request)
+        }
     }
 
-    protected async getListResults(options: any, resource: string): Promise<any> {
-        const query = this.buildListReportConfig(options, resource)
+    protected async getListResults(resource: string, options?: ServiceListOptions): Promise<any> {
+        const query = this.buildListQuery(resource, options)
         const results = await this.report(query)
         return this.parseServiceResults(results)
+    }
+
+    protected buildResource(resource: string, data: any): unknown {
+        const pb = this.client.buildResource(resource, data)
+        return pb
     }
 
     // TODO: Add support for custom metrics?
@@ -72,11 +78,16 @@ export default class Service {
         }
     }
 
-    private buildListReportConfig(options: any, resource: string): string {
+    private buildListQuery(resource: string, options?: ServiceListOptions): string {
+        if (!fields.hasOwnProperty(resource)) {
+            throw new Error(`Resource "${resource}" not found in google-ads-node compiled resources (fields.ts).`)
+        }
+        const resource_fields = (fields as any)[resource]
+
         const config: ReportConfig = {
-            attributes: options.fields,
-            constraints: options.constraints,
-            limit: options.limit,
+            attributes: resource_fields,
+            constraints: options && options.constraints ? options.constraints : [],
+            limit: options && options.limit ? options.limit : undefined,
             entity: resource,
         }
         const { query } = buildReportQuery(config)
