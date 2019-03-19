3995708190492ea9e3a9e1ad9f80afb2108f35f8
Axel Vermeil <axel.vermeil@gmail.com>
2019-03-19 17:08:21 +0000

    feat(services): added support for adgroups

--- src/test_utils.ts
+++ src/test_utils.ts
@@ -3,6 +3,7 @@ import GoogleAdsApi from './index'
 export const CID = process.env.GADS_CID as string
 export const CAMPAIGN_ID = +(process.env.GADS_CAMPAIGN_ID as string)
 export const BUDGET_ID = +(process.env.GADS_BUDGET_ID as string)
+export const ADGROUP_ID = +(process.env.GADS_ADGROUP_ID as string)
 
 export function newCustomer() {
     const client = new GoogleAdsApi({
