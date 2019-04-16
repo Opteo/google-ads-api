07b2b2964840d6512a40923acd866bd9b1c2a4fa
Louis Bluhm <suspiciouspotato@gmail.com>
2019-04-16 13:32:09 +0100

    fix(services): corrected conversion upload methods

--- src/services/conversion_upload.ts
+++ src/services/conversion_upload.ts
@@ -1,19 +1,30 @@
 import { CallConversion, ClickConversion } from 'google-ads-node/build/lib/resources'
+import * as grpc from 'google-ads-node'
 
 import Service from './service'
 
 export default class ConversionAdjustmentUploadService extends Service {
-    public async uploadCallConversion(conversion: CallConversion): Promise<any> {
-        const pb = this.buildResource('UploadCallConversionsRequest', conversion)
-        const response = await this.service.uploadConversionAdjustments(pb)
-        const parsed = this.parseServiceResults(response)
-        return parsed
+    public async uploadCallConversions(conversions: Array<CallConversion>): Promise<any> {
+        const protobufs = this.buildResources('CallConversion', conversions) as Array<grpc.CallConversion>
+
+        const request = new grpc.UploadCallConversionsRequest()
+        request.setCustomerId(this.cid)
+        request.setConversionsList(protobufs)
+        request.setPartialFailure(true)
+
+        const response = await this.serviceCall('uploadCallConversions', request, true)
+        return response
     }
 
-    public async uploadClickConversion(conversion: ClickConversion): Promise<any> {
-        const pb = this.buildResource('UploadClickConversionsRequest', conversion)
-        const response = await this.service.uploadConversionAdjustments(pb)
-        const parsed = this.parseServiceResults(response)
-        return parsed
+    public async uploadClickConversions(conversions: Array<ClickConversion>): Promise<any> {
+        const protobufs = this.buildResources('ClickConversion', conversions) as Array<grpc.ClickConversion>
+
+        const request = new grpc.UploadClickConversionsRequest()
+        request.setCustomerId(this.cid)
+        request.setConversionsList(protobufs)
+        request.setPartialFailure(true)
+
+        const response = await this.serviceCall('uploadClickConversions', request, true)
+        return response
     }
 }
