df35a4df2912bb40ab8222f95a1a7354134cc644
Mateusz Janusz <mateuszjanusz95@gmail.com>
2019-04-24 12:20:42 +0100

    added collapsible subsections in sidebar

--- docs/src/components/sidebar.js
+++ docs/src/components/sidebar.js
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const scrollToElement = require('scroll-to-element')
+import Collapsible from 'react-collapsible'
+import scrollToElement from 'scroll-to-element'
+// const scrollToElement = require('scroll-to-element')
+
 const jumpToSection = id => {
-    console.log(id)
     return scrollToElement(id, {
         offset: 0,
         duration: 600,
@@ -24,27 +26,31 @@ const Sidebar = ({ ids }) => {
                     const section_id = `#${subsections[1].id}`
 
                     return (
-                        <li key={key}>
-                            <strong className="pointer f6" onClick={() => jumpToSection(section_id)}>
-                                {entity}
-                            </strong>
-                            <ul className="list">
-                                {Object.keys(subsections).map(index => {
-                                    const sub_key = `${key}-${index}`
-                                    const subpage = subsections[index]
-                                    const subsection_id = `#${subpage.id}`
+                        <li key={key} className="mt1 mb0">
+                            <Collapsible
+                                trigger={entity}
+                                triggerClassName="pointer fw6"
+                                triggerOpenedClassName="pointer fw6"
+                                onOpening={() => jumpToSection(section_id)}
+                            >
+                                <ul className="list">
+                                    {Object.keys(subsections).map(index => {
+                                        const sub_key = `${key}-${index}`
+                                        const subpage = subsections[index]
+                                        const subsection_id = `#${subpage.id}`
 
-                                    return (
-                                        <li
-                                            className="pointer f7 fw5"
-                                            key={sub_key}
-                                            onClick={() => jumpToSection(subsection_id)}
-                                        >
-                                            {subpage.title}
-                                        </li>
-                                    )
-                                })}
-                            </ul>
+                                        return (
+                                            <li
+                                                className="pointer f7 fw5 mt1 mb0"
+                                                key={sub_key}
+                                                onClick={() => jumpToSection(subsection_id)}
+                                            >
+                                                {subpage.title}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </Collapsible>
                         </li>
                     )
                 })}
