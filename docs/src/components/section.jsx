c40184d24919bc3a454237738188456a2f818678
Axel Vermeil <axel.vermeil@gmail.com>
2019-04-29 15:29:47 +0100

    avoid rerendering componenents when clicking internal links

--- docs/src/components/section.jsx
+++ docs/src/components/section.jsx
@@ -1,30 +1,34 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 
 import AttributesTable from './attributesTable'
 
-const Section = ({ data }) => {
-    const { id, node, meta, is_index } = data
-    const { html } = node
+class Section extends React.Component {
 
-    return (
-        <div id={id} key={id} className={`  ` + (is_index ? 'bb b--opteo-light-gray pt6 pb4' : 'pv4')} >
+    shouldComponentUpdate(nextProps, nextState) {
+        return false
+    }
 
-            <div className="w-100 fl v-top">
-                <div className="f6 content v-top" dangerouslySetInnerHTML={{ __html: html }} />
-                <div className="w-50">
-                    {meta ? <AttributesTable section={id} data={meta} /> : null}
+    render(){
+        const { data } = this.props
+        const { id, node, meta, is_index } = data
+        const { html } = node
 
+        return (
+            <div id={id} key={id} className={`  ` + (is_index ? 'bb b--opteo-light-gray pt6 pb4' : 'pv4')} >
+
+                <div className="w-100 fl v-top">
+                    <div className="f6 content v-top" dangerouslySetInnerHTML={{ __html: html }} />
+                    <div className="w-50">
+                        {meta ? <AttributesTable section={id} data={meta} /> : null}
+
+                    </div>
                 </div>
+
+                <div className="cf"></div>
             </div>
-
-            <div className="cf"></div>
-        </div>
-    )
+        )
+    }
 }
 
-Section.propTypes = {
-    data: PropTypes.object.isRequired,
-}
 
 export default Section
--- docs/src/components/sidebar.jsx
+++ docs/src/components/sidebar.jsx
@@ -5,45 +5,53 @@ import { Link } from "gatsby"
 import { toggleVisible } from '../utils'
 import _ from 'lodash'
 
-const Sidebar = ({ ids }) => {
-    return (
-        <div className="absolute top-0 h-100 bg-white fixed br b--opteo-light-gray" style={{ maxWidth: '280px' }}>
-            <h3 className="tc pv3 bb b--opteo-light-gray">google-ads-api</h3>
-            <h4 className="f6 pv2 opteo-blue pl4">CORE RESOURCES</h4>
-            <ul className="h-100 list f5 ph0 overflow-y-scroll">
-                {Object.keys(ids).map(entity => {
-                    const subsections = ids[entity]
-                    const key = `sidebar-${entity}`
+class Sidebar extends React.Component {
 
-                    return (
-                        <li key={key} className="mv0 pb2">
-                            <div className="pointer opteo-blue" onClick={() => toggleVisible(key)}>
-                                {entity}
-                            </div>
+    shouldComponentUpdate(nextProps, nextState) {
+        return false
+    }
 
-                            <ul style={{ display: 'none' }} className="list" id={key}>
-                                {Object.keys(subsections).map(index => {
-                                    const sub_key = `${key}-${index}`
-                                    const subpage = subsections[index]
-                                    const subsection_id = `#${subpage.id}`
+    render(){
+        const { ids } = this.props
+        return (
+            <div className="absolute top-0 h-100 bg-white fixed br b--opteo-light-gray" style={{ maxWidth: '280px' }}>
+                <h3 className="tc pv3 bb b--opteo-light-gray">google-ads-api</h3>
+                <h4 className="f6 pv2 opteo-blue pl4">CORE RESOURCES</h4>
+                <ul className="h-100 list f5 ph0 overflow-y-scroll">
+                    {Object.keys(ids).map(entity => {
+                        const subsections = ids[entity]
+                        const key = `sidebar-${entity}`
 
-                                    return (
-                                        <li className="pointer f6 fw5 mt1 mb0" key={sub_key}>
-                                            <Link to={'/'+subsection_id}>
-                                                {subpage.type === 'object'
-                                                    ? `The ${subpage.entity} object`
-                                                    : `${_.startCase(subpage.type)}`}
-                                            </Link>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+                        return (
+                            <li key={key} className="mv0 pb2">
+                                <div className="pointer opteo-blue" onClick={() => toggleVisible(key)}>
+                                    {entity}
+                                </div>
+
+                                <ul style={{ display: 'none' }} className="list" id={key}>
+                                    {Object.keys(subsections).map(index => {
+                                        const sub_key = `${key}-${index}`
+                                        const subpage = subsections[index]
+                                        const subsection_id = `#${subpage.id}`
+
+                                        return (
+                                            <li className="pointer f6 fw5 mt1 mb0" key={sub_key}>
+                                                <Link to={'/'+subsection_id}>
+                                                    {subpage.type === 'object'
+                                                        ? `The ${subpage.entity} object`
+                                                        : `${_.startCase(subpage.type)}`}
+                                                </Link>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
 }
 
 Sidebar.propTypes = {
