3975287e9be76c7642272e2e45234ce8ef087e1f
Mateusz Janusz <mateuszjanusz95@gmail.com>
2019-05-04 10:58:07 +0100

    fixed sticky code block

--- docs/src/components/section.jsx
+++ docs/src/components/section.jsx
@@ -18,22 +18,24 @@ class Section extends React.Component {
         const html_code = code.node.html || ''
 
         return (
-            <SectionContainer
-                id={id}
-                key={id}
-                className={
-                    `  ` +
-                    (is_index
-                        ? 'flex justify-between bb b--opteo-light-gray pv5'
-                        : 'flex justify-between bb b--opteo-light-gray pv5')
-                }
-                onEnterViewport={() => this.props.onSectionChange(id)}
-            >
-                <div className="text-container w-48">
-                    <div className="content" dangerouslySetInnerHTML={{ __html: html_description }} />
-                    <div>{meta ? <AttributesTable section={id} data={meta} /> : null}</div>
+            <SectionContainer id={id} key={id} onEnterViewport={() => this.props.onSectionChange(id)}>
+                <div
+                    className={
+                        ' ' +
+                        (is_index
+                            ? 'flex justify-between bb b--opteo-light-gray pv5'
+                            : 'flex justify-between bb b--opteo-light-gray pv5')
+                    }
+                >
+                    <div className="text-container w-48">
+                        <div className="content" dangerouslySetInnerHTML={{ __html: html_description }} />
+                        <div>{meta ? <AttributesTable section={id} data={meta} /> : null}</div>
+                    </div>
+                    <div
+                        className="sticky-code-block self-start w-48 f6"
+                        dangerouslySetInnerHTML={{ __html: html_code }}
+                    />
                 </div>
-                <div className="sticky-code-block self-start w-48 f6" dangerouslySetInnerHTML={{ __html: html_code }} />
             </SectionContainer>
         )
     }
