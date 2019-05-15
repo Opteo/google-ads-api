32fed523dd2eb36e6443de5f1c4cccdf3e30eac7
Axel Vermeil <axel.vermeil@gmail.com>
2019-05-15 18:11:13 +0100

    remove stray console.log

--- docs/src/components/section.jsx
+++ docs/src/components/section.jsx
@@ -12,7 +12,6 @@ class Section extends React.Component {
         const { data } = this.props
 
         const { code, description } = data
-        console.log(data)
 
         const { id, meta, is_entity_index } = description
 
