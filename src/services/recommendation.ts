26617453fae05283b7ce7cb6cd89d271b7de4b78
jafaircl <jafaircl@me.com>
2019-10-29 11:52:43 -0400

    Add options param to applyRecommendation method

--- src/services/recommendation.ts
+++ src/services/recommendation.ts
@@ -3,7 +3,7 @@ import { Recommendation } from 'google-ads-node/build/lib/resources'
 import { ApplyRecommendationRequest, ApplyRecommendationOperation, ApplyRecommendationResponse } from 'google-ads-node'
 
 import Service from './service'
-import { ServiceListOptions } from '../types'
+import { ServiceListOptions, ServiceCreateOptions } from '../types'
 
 /**
  * @constants
@@ -25,13 +25,16 @@ export default class RecommendationService extends Service {
     public async list(options?: ServiceListOptions): Promise<Array<{ recommendation: Recommendation }>> {
         return this.getListResults('recommendation', options)
     }
-    
-    public async applyRecommendation(resourceName: string): Promise<ApplyRecommendationResponse.AsObject> {
+
+    public async applyRecommendation(
+        resourceName: string,
+        options?: ServiceCreateOptions
+    ): Promise<ApplyRecommendationResponse.AsObject> {
         const request = new ApplyRecommendationRequest()
         const operation = new ApplyRecommendationOperation()
         operation.setResourceName(resourceName)
         request.setCustomerId(this.cid)
         request.setOperationsList([operation])
-        return this.service.applyRecommendation(request)
+        return this.service.applyRecommendation(request, options)
     }
 }
