185f9402fb17c63228bc585dfe1989da88974007
jafaircl <jafaircl@me.com>
2019-10-29 11:28:21 -0400

    Create recommendation.test.ts

---
+++ src/tests/recommendation.test.ts
@@ -0,0 +1,30 @@
+// @ts-ignore
+import { newCustomerWithMetrics } from '../test_utils'
+const customer = newCustomerWithMetrics()
+
+describe('Recommendations', () => {
+    let recommendationResourceName = ''
+
+    // Even though this is in an auto-gen test, we need to get a recommendation to test applying it
+    it('Lists recommendations', async () => {
+        expect.assertions(2)
+        const recommendations = await customer.recommendations.list()
+        expect(recommendations).toBeInstanceOf(Array)
+        expect(typeof recommendations[0].recommendation.resource_name).toEqual('string')
+        recommendationResourceName = recommendations[0].recommendation.resource_name as string
+    })
+
+    it('Gets a recommendation', async () => {
+        expect.assertions(2)
+        const recommendation = await customer.recommendations.get(recommendationResourceName.split('/')[3])
+        expect(recommendation).toBeInstanceOf(Object)
+        expect(typeof recommendation.resource_name).toEqual('string')
+    })
+
+    it('Applies a recommendation', async () => {
+        expect.assertions(2)
+        const { resultsList } = await customer.recommendations.applyRecommendation(recommendationResourceName)
+        expect(resultsList instanceof Array).toEqual(true)
+        expect(resultsList[0]).toHaveProperty('resourceName')
+    })
+})
