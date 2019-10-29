46a88a993ab2d62fe324f159caace48ac67cc98c
jafaircl <jafaircl@me.com>
2019-10-29 13:57:47 -0400

    Update recommendation.test.ts

--- src/tests/recommendation.test.ts
+++ src/tests/recommendation.test.ts
@@ -9,6 +9,7 @@ describe('Recommendations', () => {
     it('Lists recommendations', async () => {
         expect.assertions(2)
         const recommendations = await customer.recommendations.list()
+        console.log(recommendations)
         expect(recommendations).toBeInstanceOf(Array)
         expect(typeof recommendations[0].recommendation.resource_name).toEqual('string')
         recommendationResourceName = recommendations[0].recommendation.resource_name as string
@@ -21,10 +22,20 @@ describe('Recommendations', () => {
         expect(typeof recommendation.resource_name).toEqual('string')
     })
 
+    /*
+    These are commented out to make sure no changes are actually made to accounts
+    
     it('Applies a recommendation', async () => {
         expect.assertions(2)
         const { results } = await customer.recommendations.applyRecommendation(recommendationResourceName)
         expect(results instanceof Array).toEqual(true)
         expect(typeof results[0]).toEqual('string')
     })
+
+    it('Dismisses a recommendation', async () => {
+        expect.assertions(2)
+        const { results } = await customer.recommendations.dismissRecommendation(recommendationResourceName)
+        expect(results instanceof Array).toEqual(true)
+        expect(typeof results[0]).toEqual('string')
+    }) */
 })
