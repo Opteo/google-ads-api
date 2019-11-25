e9a8c7de5004a2dacb034f7059b7fe722eaaa540
jafaircl <jafaircl@me.com>
2019-11-25 07:14:02 -0500

    feat(services): added recommendation service methods (#84)
    * Create recommendation.test.ts
    
    * Add applyRecommendation method
    
    * Add options param to applyRecommendation method
    
    * add validate_only to applyRecommendation test
    
    * Remove validate_only since it doesn't do anything
    
    * ApplyRecommendationRequest has no setValidateOnly method
    
    * Standardize output from applyRecommendation
    
    * add dismissRecommendation
    
    * Update recommendation.test.ts
    
    * Update recommendation.test.ts
    
    * allow an array of resource names

--- src/services/recommendation.ts
+++ src/services/recommendation.ts
@@ -1,8 +1,15 @@
 // @ts-ignore
 import { Recommendation } from 'google-ads-node/build/lib/resources'
+import {
+    ApplyRecommendationRequest,
+    ApplyRecommendationOperation,
+    ApplyRecommendationResponse,
+    DismissRecommendationRequest,
+    DismissRecommendationResponse,
+} from 'google-ads-node'
 
-import Service from './service'
-import { ServiceListOptions } from '../types'
+import Service, { Mutation } from './service'
+import { ServiceListOptions, ServiceCreateOptions } from '../types'
 
 /**
  * @constants
@@ -23,5 +30,75 @@ export default class RecommendationService extends Service {
 
     public async list(options?: ServiceListOptions): Promise<Array<{ recommendation: Recommendation }>> {
         return this.getListResults('recommendation', options)
+    }
+
+    public async applyRecommendation(
+        resourceName: string | string[],
+        options?: ServiceCreateOptions
+    ): Promise<Mutation> {
+        const request = new ApplyRecommendationRequest()
+        request.setCustomerId(this.cid)
+
+        if (resourceName instanceof Array) {
+            const operations = resourceName.map(name => {
+                const operation = new ApplyRecommendationOperation()
+                operation.setResourceName(name)
+                return operation
+            })
+            request.setOperationsList(operations)
+        } else {
+            const operation = new ApplyRecommendationOperation()
+            operation.setResourceName(resourceName)
+            request.setOperationsList([operation])
+        }
+
+        if (options && options.hasOwnProperty('partial_failure')) {
+            if (!request.setPartialFailure) {
+                throw new Error(`This method does not support the partial_failure option.`)
+            }
+            request.setPartialFailure(options.partial_failure as boolean)
+        }
+
+        const response: ApplyRecommendationResponse.AsObject = await this.service.applyRecommendation(request)
+        return {
+            request: request.toObject(),
+            partial_failure_error: response.partialFailureError,
+            results: response.resultsList.map(r => r.resourceName),
+        }
+    }
+
+    public async dismissRecommendation(
+        resourceName: string | string[],
+        options?: ServiceCreateOptions
+    ): Promise<Mutation> {
+        const request = new DismissRecommendationRequest()
+        request.setCustomerId(this.cid)
+
+        if (resourceName instanceof Array) {
+            const operations = resourceName.map(name => {
+                const operation = new DismissRecommendationRequest.DismissRecommendationOperation()
+                operation.setResourceName(name)
+                return operation
+            })
+            request.setOperationsList(operations)
+        } else {
+            const operation = new DismissRecommendationRequest.DismissRecommendationOperation()
+            operation.setResourceName(resourceName)
+            request.setOperationsList([operation])
+        }
+
+        if (options && options.hasOwnProperty('partial_failure')) {
+            if (!request.setPartialFailure) {
+                throw new Error(`This method does not support the partial_failure option.`)
+            }
+            request.setPartialFailure(options.partial_failure as boolean)
+        }
+
+        const response: DismissRecommendationResponse.AsObject = await this.service.dismissRecommendation(request)
+        return {
+            request: request.toObject(),
+            partial_failure_error: response.partialFailureError,
+            results: response.resultsList.map(r => r.resourceName),
+        }
     }
 }
